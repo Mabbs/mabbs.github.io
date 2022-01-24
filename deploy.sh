f9e8efcc7935f1be5b0257a7fc89d55ef25b3fce
mayx <mayx@outlook.com>
2022-01-24 15:20:15 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ layout: default
 ---" > Mabbs/index.md
 cat Mabbs.md >> Mabbs/index.md
 rm -rf Mabbs.md
-rm -rf .git/
 bundle exec jekyll build -d public
-rm -rf .jekyll-cache/
 tar czvf MayxBlog.tgz public/
 mv MayxBlog.tgz public/
