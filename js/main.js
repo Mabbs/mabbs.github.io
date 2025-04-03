b0704e26fcd55ae2402222ef705b2c201b06045a
mayx <mayx@outlook.com>
2025-04-03 14:38:40 +0000

    Update 6 files
    - /js/main.js
    - /search.html
    - /_layouts/default.html
    - /_layouts/post.html
    - /_includes/anchor_headings.html
    - /_config.yml

--- js/main.js
+++ js/main.js
@@ -42,16 +42,8 @@ $(function () {
 });
 
 $(function() {
-    function getQueryVariable(variable){
-           var query = window.location.search.substring(1);
-           var vars = query.split("&");
-           for (var i=0;i<vars.length;i++) {
-                   var pair = vars[i].split("=");
-                   if(pair[0] == variable){return pair[1];}
-           }
-           return(false);
-    }
-    var keyword = decodeURI(getQueryVariable("kw"));
+    const urlParams = new URLSearchParams(window.location.search);
+    const keyword = urlParams.get('kw')?.trim();
 
     if (!keyword) return;
 
